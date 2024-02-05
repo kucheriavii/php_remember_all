515127184dbee846eea441aacadc5cd75d17bc24
r.ostapchuk <kucheriavii@gmail.com>
2024-02-05 21:19:33 +0200

    first commit~

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+  <?php
+    echo "Hello world";
+  ?>
+
+  <?php
+    $age = 40;
+    print_r(++$age);
+    $test = [10,1,12,13,14,15];
+    var_dump($test);
+  
+  ?>
+
+
+</html>
