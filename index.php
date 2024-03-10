f734cb8c398bf822cd899fe2f16fdcee46c566f7
r.ostapchuk <kucheriavii@gmail.com>
2024-03-10 19:49:22 +0200

    php_remember_all

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<!doctype html>
-<html lang="en">
-
-  <?php
-    echo "Hello world";
-    
-  ?>
-
-  <?php
-    $age = 40;
-    print_r(++$age);
-    $test = [10,1,12,13,14,15];
-    var_dump($test);
-  
-  ?>
-
-
-</html>
+<?php ?>
+<h1><?php ?></h1>
+<?php ?>
