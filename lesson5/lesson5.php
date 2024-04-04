58f5a3282e7ba1878582d678d8a2425a87c5da03
r.ostapchuk <kucheriavii@gmail.com>
2024-04-04 21:42:18 +0300

    5. Rounding Numbers.mp4

---
+++ lesson5/lesson5.php
@@ -0,0 +1,12 @@
+<?php 
+    declare(strict_types = 1);
+?>
+<h2>Lesson 5</h2>
+<a href="/php_remember_all">Back home</a>
+<br />
+<br />
+<?php   
+    echo round(5.545, 0)."<br>";
+    echo floor(5.545)."<br>";
+    echo ceil(5.545)."<br>";
+    echo round(5.5, 0, PHP_ROUND_HALF_DOWN)."<br>";
