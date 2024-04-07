918b1037401f6d6f85cb47b96264d1e99d24df65
r.ostapchuk <kucheriavii@gmail.com>
2024-04-07 20:32:51 +0300

    16. Array Functions.mp4

---
+++ lesson5/lesson15.php
@@ -0,0 +1,17 @@
+<h2>Lesson 14</h2>
+<a href="/php_remember_all">Back home</a>
+<br />
+<br />
+<?php 
+    $myCup = 'empty';
+    $myFriendsCup = 'empty';
+
+    function fillCup(&$cupParam){
+        $cupParam = 'filled';
+    }
+
+    fillCup($myCup);
+    fillCup($myFriendsCup);
+
+    echo $myCup;
+    echo $myFriendsCup;
