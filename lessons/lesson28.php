6dbe76a14ef6484b256393f19bc4483a7317e3a4
r.ostapchuk <kucheriavii@gmail.com>
2024-04-21 17:32:58 +0300

    31. Working With File System In PHP - Full PHP 8 Tutorial

--- lessons/lesson28.php
+++ lessons/lesson28.php
@@ -1,5 +1,6 @@
 <?php 
     declare(strict_types=1);
 ?>
-<h1>Lesson 24</h1>
+<h1>Lesson 28</h1>
 <?php
+error_reporting(E_ALL & ~E_WARNING);
---
+++ lessons/lesson30.php
@@ -0,0 +1,33 @@
+<?php 
+    declare(strict_types=1);
+?>
+<h1>Lesson 30</h1>
+<?php
+
+    // $dir = scandir(__DIR__);
+    // var_dump($dir[2]);
+    // mkdir('foo/bar', recursive: true);
+    
+    // // rmdir('foo/bar');
+    // rmdir('foo');
+
+    // if (file_exists("./foo.txt")){
+    //     echo filesize("./foo.txt");
+    //     file_put_contents('./foo.txt', "Hello world");
+
+    // } else {
+    //     echo "File not found";
+    // }
+
+    // $file = @fopen('./foo.txt', 'r');
+    
+    // while(($line = fgets($file)) !== false){
+    //     echo $line . "<br>";
+    // }
+
+    // fclose($file);
+
+    // file_put_contents("bar.txt", "Hello ");
+    // file_put_contents("bar.txt", "world", FILE_APPEND);
+    // copy('foo.txt', 'test.txt');
+    // rename('test.txt', '1.txt');
